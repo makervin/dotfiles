08b1bf6fe496c884d242c5bf120c3f7c261dbcb0
Vincent Ockers <vincentbockers@gmail.com>
2024-07-23 16:08:29 +0200

    Add .tmux.conf

--- scripts/sync-local.sh
+++ scripts/sync-local.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-rsync -a --mkpath ./.config/fish/config.fish ~/.config/fish/config.fish
-rsync -a --mkpath ./.zshrc ~/.zshrc 
-rsync -a --mkpath --exclude=lazy-lock.json .config/nvim/ ~/.config/nvim
+rsync -a --exclude=".git" ./ ~/
