332a2c4aa91c3bced5a878ea3806bb266a4738ed
Vincent Ockers <vincentbockers@gmail.com>
2024-07-25 22:15:47 +0200

    Add alacritty.toml

--- scripts/sync-remote.sh
+++ scripts/sync-remote.sh
@@ -4,3 +4,4 @@ rsync -a --mkpath ~/.config/fish/config.fish ./.config/fish/config.fish
 rsync -a --mkpath ~/.zshrc ./.zshrc
 rsync -a --mkpath --exclude=lazy-lock.json ~/.config/nvim/ .config/nvim
 rsync -a --mkpath ~/.config/i3/ .config/i3
+rsync -a --mkpath ~/.config/alacritty/alacritty.toml .config/alacritty/alacritty.toml
