d0c6e94a75b907c519dde061d46618a9165c98fb
Vincent Ockers <vincentbockers@gmail.com>
2024-09-02 17:56:48 +0200

    Add svelte

---
+++ .config/nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,3 @@
+local o = vim.o
+o.tabstop = 4
+o.shiftwidth = 0
--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -5,15 +5,19 @@ local options = {
     html = { "prettier" },
     javascript = { "prettier" },
     typescript = { "prettier" },
+    svelte = { "prettier" },
+    c = { "clang_format" },
+    cpp = { "clang_format" },
+    rust = { "rustfmt" },
   },
 
-  format_on_save = function(bufnr)
-    -- Disable with a global or buffer-local variable
-    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-      return
-    end
-    return { timeout_ms = 500, lsp_format = "fallback" }
-  end,
+  -- format_on_save = function(bufnr)
+  --   -- Disable with a global or buffer-local variable
+  --   if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+  --     return
+  --   end
+  --   return { timeout_ms = 500, lsp_format = "fallback" }
+  -- end,
 }
 
 require("conform").setup(options)
